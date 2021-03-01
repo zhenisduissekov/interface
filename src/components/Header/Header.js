bda137db696f15672f1f8f0caadb873d4a7d622a
zhenisduissekov <zduissekov@gmail.com>
2021-03-01 14:59:30 +0600

    added buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import { IconContext } from "react-icons";
 import * as FaIcons from "react-icons/fa";
 import s from "./Header.module.css";
+// import { AwesomeButton } from "react-awesome-button";
+// import "react-awesome-button/dist/styles.css";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
 export default class Header extends Component {
   constructor(props) {
@@ -17,9 +22,28 @@ export default class Header extends Component {
             value={{ style: { fontSize: "40px", color: "rgb(0, 123, 255)" } }}
           >
             <FaIcons.FaBars onClick={this.props.sidebarToggle} />
-            <button className={s.btn} onClick={this.props.button}>
+            <Button
+              variant="contained"
+              color="secondary"
+              size="medium"
+              onClick={this.props.button1}
+            >
+              SAVE/OVERWRITE
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.props.button2}
+            >
+              DELETE
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.props.button3}
+            >
               Reset Prometheus
-            </button>
+            </Button>
           </IconContext.Provider>
           <div className={s.clock}>{this.state.date.toLocaleTimeString()}</div>
         </div>
